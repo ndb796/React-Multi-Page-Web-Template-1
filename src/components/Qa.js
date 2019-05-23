192a69d04926a18297ce9f49c38824dee4bb305c
ndb796 <ndb796@naver.com>
2019-05-23 11:50:22 +0900

    Add Qa Design

--- src/components/Qa.js
+++ src/components/Qa.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 import './Qa.css';
+import arrowIcon from '../icons/arrow_down.png';
 
 class Qa extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+    toggle = () => {
+        const currentState = this.state.active;
+        this.setState({ active: !currentState })
+    }
     render() {
         return (
             <div className="qa">
                 <div className="text">
-                    질문: {this.props.question}<br/>
-                    답변: {this.props.answer}
+                    <div className="question">
+                        질문: {this.props.question}
+                        <div className="icon">
+                            <img src={arrowIcon} onClick={this.toggle}/>
+                        </div>
+                    </div>
+                    <div
+                        className={this.state.active ? 'answer' : 'hide'}>
+                        답변: {this.props.answer}
+                    </div>
                 </div>
             </div>
         );

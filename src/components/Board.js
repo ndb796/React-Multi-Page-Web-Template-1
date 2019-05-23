f794ffff1d5c03a171640ab27ce094ee150a0790
ndb796 <ndb796@naver.com>
2019-05-23 13:52:12 +0900

    Add Board Module

---
+++ src/components/Board.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Board.css';
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false
+        };
+    }
+    hover = () => {
+        this.setState({ hover: true })
+    }
+    leave = () => {
+        this.setState({ hover: false })
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="box">
+                    <div
+                        onMouseEnter={this.hover}
+                        onMouseLeave={this.leave}
+                        className={this.state.hover ? "content hover" : "content"}>
+                        {this.props.id}
+                        <br />
+                        {this.props.title}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Board from '../components/Board';
+
+const list = [
+  {
+    id: 1,
+    title: '제목 1'
+  },
+  {
+    id: 2,
+    title: '제목 2'
+  },
+  {
+    id: 3,
+    title: '제목 3'
+  },
+  {
+    id: 4,
+    title: '제목 4'
+  },
+  {
+    id: 5,
+    title: '제목 5'
+  },
+  {
+    id: 6,
+    title: '제목 6'
+  }
+]
 
 function About() {
   return (
     <div>
-        About
+      {list.map(board => {
+        return <Board key={board.id} id={board.id} title={board.title}/>
+      })}
     </div>
   );
 }
